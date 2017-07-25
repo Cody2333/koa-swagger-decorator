6e420427513f051ebdc41bae580e1c128417e98a
sway <swaylq0913@gmail.com>
2017-07-26 04:43:43 +0800

    [RECOVER] index.js

--- dist/index.js
+++ dist/index.js
@@ -3,27 +3,94 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-/**
- * swagger的初始化模板
- * @param {String} title
- * @param {String} description
- * @param {String} version
- */
+exports.apiObjects = exports.wrapper = exports.tags = exports.body = exports.path = exports.query = exports.description = exports.desc = exports.params = exports.summary = exports.request = undefined;
 
-const init = (title = 'API DOC', description = 'API DOC', version = '1.0.0') => ({
-  info: { title, description, version },
-  definitions: {},
-  securityDefinitions: {
-    ApiKeyAuth: {
-      type: 'apiKey',
-      in: 'header',
-      name: 'Authorization'
-    }
-  },
-  paths: {},
-  responses: {},
-  swagger: '2.0',
-  tags: []
-});
+var _lodash = require('lodash');
 
-exports.init = init;
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _wrapper = require('./wrapper');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const apiObjects = {};
+
+const _addToApiObject = (target, name, apiObjects, content) => {
+  const key = `${target.name}-${name}`;
+  if (!apiObjects[key]) apiObjects[key] = {};
+  Object.assign(apiObjects[key], content);
+};
+
+const _desc = (type, text) => (target, name, descriptor) => {
+  // 设置简介
+  descriptor.value[type] = text;
+  _addToApiObject(target, name, apiObjects, { [type]: text });
+  return descriptor;
+};
+
+const _params = (type, parameters) => (target, name, descriptor) => {
+  if (!descriptor.value.parameters) descriptor.value.parameters = {};
+  descriptor.value.parameters[type] = parameters;
+
+  // 对 body 要再裹一层
+  let swaggerParameters = parameters;
+  if (type === 'body') {
+    swaggerParameters = [{
+      name: 'data',
+      description: '请求体',
+      schema: {
+        type: 'object',
+        properties: parameters
+      }
+    }];
+  } else {
+    swaggerParameters = Object.keys(swaggerParameters).map(key => Object.assign({ name: key }, swaggerParameters[key]));
+  }
+  swaggerParameters.forEach(item => {
+    item.in = type;
+  });
+
+  _addToApiObject(target, name, apiObjects, { [type]: swaggerParameters });
+  return descriptor;
+};
+
+const request = (method, path) => (target, name, descriptor) => {
+  // 设置请求方法,路径
+  descriptor.value.method = method;
+  descriptor.value.path = path;
+  _addToApiObject(target, name, apiObjects, { request: { method, path }, security: [{ ApiKeyAuth: [] }] });
+  return descriptor;
+};
+
+const desc = _lodash2.default.curry(_desc);
+// description 和 summary 参数
+
+const description = desc('description');
+
+const summary = desc('summary');
+
+const tags = desc('tags');
+
+const params = _lodash2.default.curry(_params);
+// 下面是parameters参数
+
+// query 参数
+const query = params('query');
+
+// path 参数
+const path = params('path');
+
+// body 参数
+const body = params('body');
+
+exports.request = request;
+exports.summary = summary;
+exports.params = params;
+exports.desc = desc;
+exports.description = description;
+exports.query = query;
+exports.path = path;
+exports.body = body;
+exports.tags = tags;
+exports.wrapper = _wrapper.wrapper;
+exports.apiObjects = apiObjects;
