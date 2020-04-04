36504ab312232cf15994b52f58b02d8b54000e2b
cody <wangkedi.wang@gmail.com>
2020-04-04 11:03:38 +0800

    chore: using commitlint

--- dist/swaggerPropertyHelper.d.ts
+++ dist/swaggerPropertyHelper.d.ts
@@ -2,43 +2,54 @@
  * All other regular column types.
  */
 export declare type PropertyType = "string" | "number" | "boolean" | "array" | "object";
-/**
- *
- */
-export declare class PropertyOptions {
-    /**
-     *
-     */
-    type: PropertyType;
-    /**
-     *
-     */
+declare type CustromStringFormatT = string;
+export declare type StringFormatT = "date" | "date-time" | "password" | "byte" | "binary" | CustromStringFormatT;
+declare class BasePropertyOptions {
     required?: boolean;
-    /**
-     *
-     */
     example?: any;
-    /**
-     *
-     */
     description?: string;
-    /**
-     *
-     */
-    items?: PropertyOptions;
-    /**
-     *
-     */
-    properties?: any;
+    readOnly?: boolean;
+    writeOnly?: boolean;
+    nullable?: boolean;
 }
+declare type StringPropertyOptionsT = BasePropertyOptions & {
+    type: "string";
+    minLength?: number;
+    maxLength?: number;
+    format?: StringFormatT;
+    pattern?: string;
+};
+declare type NumberPropertyOptionsT = BasePropertyOptions & {
+    type: "number";
+    minimum?: number;
+    exclusiveMinimum?: boolean;
+    maximum?: number;
+    exclusiveMaximum?: boolean;
+    multipleOf?: number;
+};
+declare type BooleanPropertyOptionsT = BasePropertyOptions & {
+    type: "boolean";
+};
+declare type ArrayPropertyOptionsT = BasePropertyOptions & {
+    type: "array";
+    minItems?: number;
+    maxItems?: number;
+    uniqueItems?: boolean;
+    items?: PropertyOptions;
+};
+declare type ObjectPropertyOptionsT = BasePropertyOptions & {
+    type: "object";
+    properties?: any;
+    minProperties?: number;
+    maxProperties?: number;
+};
+export declare type PropertyOptions = StringPropertyOptionsT | NumberPropertyOptionsT | BooleanPropertyOptionsT | ArrayPropertyOptionsT | ObjectPropertyOptionsT;
 /**
  * Made for empty class
- * @param constructor
  */
-export declare function swaggerClass(constructor?: Function): Function;
+export declare function swaggerClass(): Function;
 /**
- *
- * @param type
  * @param options
  */
 export declare function swaggerProperty(options?: PropertyOptions): Function;
+export {};
