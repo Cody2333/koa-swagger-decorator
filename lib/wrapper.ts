9442de5630f425cbeb9cdb76d656ad40f26f2e5e
wangkedi.cody <wangkedi.cody@bytedance.com>
2020-10-16 15:36:40 +0800

    refactor: wrapperMethod

--- lib/wrapper.ts
+++ lib/wrapper.ts
@@ -158,15 +158,17 @@ const handleMap = (
         await c[method](ctx);
       };
       // 添加了一层 wrapper 之后，需要把原函数的名称暴露出来 fnName
-      // hack: 将 function.name 改为可写，防止意外报错
-      Object.defineProperty(wrapperMethod, 'name', {
-        writable: true,
-        configurable: true,
-        enumerable: true
+      // wrapperMethod 继承原函数的 descriptors
+      const descriptors = Object.getOwnPropertyDescriptors(SwaggerClassPrototype[method]);
+      Object.defineProperties(wrapperMethod, {
+        fnName: {
+          value: method,
+          enumerable: true,
+          writable: true,
+          configurable: true,
+        },
+        ...descriptors
       })
-      Object.assign(wrapperMethod, SwaggerClassPrototype[method], {
-        fnName: method,
-      });
       return wrapperMethod;
     });
 
